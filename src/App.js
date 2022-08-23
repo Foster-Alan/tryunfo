4655bb47333d595ea2695a3588f23c607b499158
Alan Foster <alan-af@outlook.com>
2022-08-23 18:36:35 -0300

    5

--- src/App.js
+++ src/App.js
@@ -18,7 +18,34 @@ class App extends React.Component {
   onInputChange = ({ target }) => {
     const { name, type } = target;
     const value = type === 'checkbox' ? target.checked : target.value;
-    this.setState({ [name]: value });
+    this.setState({ [name]: value }, () => this.verifyteSaveButton());
+  };
+
+  verifyteSaveButton = () => {
+    if (this.verifyAttribute() && this.verifyText()) {
+      return this.setState({ isSaveButtonDisabled: false });
+    }
+    return this.setState({ isSaveButtonDisabled: true });
+  };
+
+  verifyAttribute = () => {
+    const { cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    const attr1 = Number(cardAttr1);
+    const attr2 = Number(cardAttr2);
+    const attr3 = Number(cardAttr3);
+    const sumAttrs = attr1 + attr2 + attr3;
+    const maxAttValue = 90;
+    const maxSumAtt = 210;
+
+    return !(attr1 > maxAttValue || attr1 < 0
+      || attr2 > maxAttValue || attr2 < 0
+      || attr3 > maxAttValue || attr3 < 0
+      || sumAttrs > maxSumAtt);
+  };
+
+  verifyText = () => {
+    const { cardName, cardDescription, cardImage } = this.state;
+    return !(cardName === '' || cardDescription === '' || cardImage === '');
   };
 
   render() {
